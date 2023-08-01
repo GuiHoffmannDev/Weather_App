3f46176e226e3869f69a40f359cb0ee5ba7075e6
GuiHoffmannDev <guihoffmann.dev@outlook.com>
2023-08-01 11:01:29 -0300

    Initial Commit

---
+++ WeatherApp/Components/SwiftUIVieWeather.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIVieWeather.swift
+//  WeatherApp
+//
+//  Created by Guilherme on 01/08/23.
+//
+
+import SwiftUI
+
+struct SwiftUIVieWeather: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIVieWeather_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIVieWeather()
+    }
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Guilherme on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
